ee683d1e88513e733838806741e3822c56f0698e
linnvassvik <linnvassvik@gmail.com>
2022-12-01 15:42:51 +0100

    Upload data and start a script

---
+++ R/1_Import_AppleQualityData.R
@@ -0,0 +1,7 @@
+#library(viridis)
+#library(RColorBrewer)
+#library(ggpp)
+library(tidyverse)
+
+#Upload data on effects of pollination on apple quality under three different 
+#treatments: supplemental pollination, natural pollination, pollinators excluded
