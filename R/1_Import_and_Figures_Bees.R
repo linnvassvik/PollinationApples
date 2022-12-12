fa8c41866c3a9b82a6289b085074255c9fc0abbe
linnvassvik <linnvassvik@gmail.com>
2022-12-12 16:53:50 +0100

    Created figures for christmas letter/update

--- R/1_Import_and_Figures_Bees.R
+++ R/1_Import_and_Figures_Bees.R
@@ -3,15 +3,17 @@
 library(ggpp)
 library(tidyverse)
 library(readxl)
-library(ggpattern)
+
 
 ## IMPORT DATASET
 BeesWest <- read_excel("Data/Bees_Vestlandet_SiljeOgSara.xlsx")
 
 
 
-ggplot(BeesWest) +
-  geom_bar(aes(x = Norsk_gruppe, y = after_stat(count), fill = Norsk_gruppe)) +
+PollinatorsWest <- BeesWest %>% 
+  mutate(across(Norsk_gruppe, factor, levels=c("Honningbie","Humle","Villbie", "Blomsterflue", "Veps"))) %>%
+  ggplot(aes(x = Norsk_gruppe, y = after_stat(count), fill = Norsk_gruppe)) +
+  geom_bar() +
   facet_wrap(~ Location) +
   theme(strip.background = element_blank()) +
   theme(axis.text.x = element_text(angle = 35, size = 12, vjust = 1, hjust = 1),
@@ -23,6 +25,7 @@ ggplot(BeesWest) +
         legend.position = "none",
         plot.title = element_text (hjust = 0.5, size = 20, face = "bold")) +
   labs(y="Antall", x="", fill="Pollinatorer") +
-  scale_fill_manual(values = c("#996600", "#336633", "#CC3300", "#FF9900", "#CC3300")) +
+  scale_fill_manual(values = c("#006600", "#669966", "#993333", "#CC6666", "#FF9900")) +
   ggtitle("Antall pollinatorer på Vestlandet")
+ggsave(PollinatorsWest, filename = "Figures/Pollinators_WestNorway.jpeg", height = 6, width = 8)
 
