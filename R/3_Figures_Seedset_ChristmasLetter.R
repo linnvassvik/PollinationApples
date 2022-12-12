28b679d5a7de504c3a9a9fdee665f25f90ec22f8
linnvassvik <linnvassvik@gmail.com>
2022-12-12 11:19:37 +0100

    Making figures for christmas letter/update

--- R/3_Figures_Seedset_ChristmasLetter.R
+++ R/3_Figures_Seedset_ChristmasLetter.R
@@ -111,6 +111,7 @@ ggsave(SummerredUllensvangPlot, filename = "Figures/Summerred_Ullensvang.jpeg", 
 ####################################################################################################################
 
 ### HOW NUMBER OF SEEDS AFFECTS APPLE WEIGHT UNDER DIFFERENT TREATMENT AND COMBINED ###
+#Dashed line are lower priced apples, solid line are higher priced apples
 
 AppleWeightSeeds <- AppleQuality %>% 
   ggplot(aes(y = Weight, x = Seeds_fully_developed, color = Treatment)) +
@@ -122,11 +123,45 @@ AppleWeightSeeds <- AppleQuality %>%
   scale_fill_manual(labels = c("Pollinatorer ekskludert (C)", "Håndpollinert (HP)", "Naturlig pollinert (N)"), values = c("#996600", "#336633", "#CC3300")) +
   ylim(0, 350) + #REMOVED TWO OUTLIERS
   ggtitle("Korrelasjon mellom vekt og antall frø") +
+  theme(strip.background = element_blank(), text = element_text(size = 25, hjust = 0.5))+
+  geom_hline(yintercept=95, linetype="dashed", color = "black", size=0.5) +
+  geom_hline(yintercept=135.5, linetype="dashed", color = "black", size=0.5) + #line taken from internal apple quality measurement assesment
+  geom_hline(yintercept=134, linetype="solid", color = "black", size=0.5) +
+  geom_hline(yintercept=275, linetype="solid", color = "black", size=0.5) +
+  facet_wrap(~ Apple_variety) +
+  theme(legend.position = "bottom", 
+        legend.title = element_text(size = 15),
+        legend.text = element_text (size = 12),
+        axis.text = element_text (size = 18),
+        axis.title = element_text (size = 18),
+        plot.title = element_text (hjust = 0.5, size = 20, face = "bold"))
+ggsave(AppleWeightSeeds, filename = "Figures/Apple_Weight_Seeds.jpeg", height = 6, width = 8)
+
+AppleWeightSeeds_Location <- AppleQuality %>% 
+  ggplot(aes(y = Weight, x = Seeds_fully_developed, color = Apple_variety)) +
+  geom_point(alpha = 0.3, position = position_jitter()) +
+  labs(x = "Antall frø", y = "Vekt (g)", color = "Eplesort", fill = "Eplesort") +
+  geom_smooth(method = lm, fullrange=FALSE, aes(fill=Apple_variety)) + #remove line with se=FALSE, small dataset remove method=lm, 
+  theme_classic() +
+  scale_color_manual (values = c("#996600", "#336633", "#CC3300")) +
+  scale_fill_manual(values = c("#996600", "#336633", "#CC3300")) +
+  ylim(0, 350) + #REMOVED TWO OUTLIERS
+  ggtitle("Korrelasjon mellom vekt og antall frø") +
   theme(plot.title = element_text(size = 18, hjust = 0.5)) +
   theme(strip.background = element_blank(), text = element_text(size = 25, hjust = 0.5))+
-  geom_hline(yintercept=90, linetype="dashed", color = "grey", size=1) + #line taken from internal apple quality measurement asssessment
-  facet_wrap(~ Apple_variety)
-ggsave(AppleWeightSeeds, filename = "Figures/Apple_Weight_Seeds.jpeg", height = 6, width = 8)
+  geom_hline(yintercept=95, linetype="dashed", color = "black", size=0.5) +
+  geom_hline(yintercept=135.5, linetype="dashed", color = "black", size=0.5) + #line taken from internal apple quality measurement assesment
+  geom_hline(yintercept=134, linetype="solid", color = "black", size=0.5) +
+  geom_hline(yintercept=275, linetype="solid", color = "black", size=0.5) +
+  facet_wrap(~ Location) +
+  theme(legend.position = "bottom", 
+        legend.title = element_text(size = 15),
+        legend.text = element_text (size = 12),
+        axis.text = element_text (size = 18),
+        axis.title = element_text (size = 18),
+        plot.title = element_text (hjust = 0.5, size = 20, face = "bold"))
+ggsave(AppleWeightSeeds_Location, filename = "Figures/Apple_Weight_Seeds_Location.jpeg", height = 6, width = 8)
+
 
 
 ### HOW NUMBER OF SEEDS AFFECTS APPLE RATIO (HEIGHT/DIAMETER) UNDER DIFFERENT TREATMENT AND COMBINED ###
@@ -134,7 +169,6 @@ ggsave(AppleWeightSeeds, filename = "Figures/Apple_Weight_Seeds.jpeg", height = 
 AppleRatioSeeds <- AppleQuality %>% 
   ggplot(aes(y = Ratio, x = Seeds_fully_developed, color = Treatment)) +
   geom_point(alpha = 0.3, position = position_jitter()) +
-  geom_hline(yintercept=1, linetype="dashed", color = "grey", size=1) +
   labs(x = "Antall frø", y = "Ratio", color = "Behandling", fill = "Behandling") +
   geom_smooth(method = lm, fullrange=FALSE, aes(fill=Treatment)) + #remove line with se=FALSE, small dataset remove method=lm, 
   theme_classic() +
@@ -142,9 +176,14 @@ AppleRatioSeeds <- AppleQuality %>%
   scale_fill_manual(values = c("#996600", "#336633", "#CC3300")) +
   ylim(0.65, 1.35) + #REMOVED TWO OUTLIERS
   ggtitle("Korrelasjon mellom ratio (høyde/diameter) og antall frø") +
-  theme(plot.title = element_text(size = 18, hjust = 0.5)) +
   theme(strip.background = element_blank(), text = element_text(size = 25, hjust = 0.5))+
-  facet_wrap(~ Apple_variety)
+  facet_wrap(~ Apple_variety) +
+  theme(legend.position = "bottom", 
+        legend.title = element_text(size = 15),
+        legend.text = element_text (size = 14),
+        axis.text = element_text (size = 18),
+        axis.title = element_text (size = 18),
+        plot.title = element_text (hjust = 0.5, size = 20))
 ggsave(AppleRatioSeeds, filename = "Figures/Apple_Ratio_Seeds.jpeg", height = 6, width = 8)
 
 
@@ -159,13 +198,19 @@ AppleHeightSeeds <- AppleQuality %>%
   scale_color_manual (values = c("#996600", "#336633", "#CC3300")) +
   scale_fill_manual(values = c("#996600", "#336633", "#CC3300")) +
   ggtitle("Korrelasjon mellom høyde og antall frø") +
-  theme(plot.title = element_text(size = 18, hjust = 0.5)) +
   theme(strip.background = element_blank(), text = element_text(size = 25, hjust = 0.5))+
-  facet_wrap(~ Apple_variety)
+  facet_wrap(~ Apple_variety) +
+  theme(legend.position = "bottom", 
+        legend.title = element_text(size = 15),
+        legend.text = element_text (size = 14),
+        axis.text = element_text (size = 18),
+        axis.title = element_text (size = 18),
+        plot.title = element_text (hjust = 0.5, size = 20))
 ggsave(AppleHeightSeeds, filename = "Figures/Apple_Height_Seeds.jpeg", height = 6, width = 8)
 
 
 ### HOW NUMBER OF SEEDS AFFECTS APPLE DIAMETER UNDER DIFFERENT TREATMENT AND COMBINED ###
+#Dashed line are lower priced apples, solid line are higher priced apples
 
 AppleDiameterSeeds <- AppleQuality %>% 
   ggplot(aes(y = Diameter, x = Seeds_fully_developed, color = Treatment)) +
@@ -173,13 +218,47 @@ AppleDiameterSeeds <- AppleQuality %>%
   labs(x = "Antall frø", y = "Diameter (mm)", color = "Behandling", fill = "Behandling") +
   geom_smooth(method = lm, fullrange=FALSE, aes(fill=Treatment)) + #remove line with se=FALSE, small dataset remove method=lm, 
   theme_classic() +
+  scale_color_manual (labels = c("Pollinatorer ekskludert (C)", "Håndpollinert (HP)", "Naturlig pollinert (N)"), values = c("#996600", "#336633", "#CC3300")) +
+  scale_fill_manual(labels = c("Pollinatorer ekskludert (C)", "Håndpollinert (HP)", "Naturlig pollinert (N)"), values = c("#996600", "#336633", "#CC3300")) +
+  ylim(30, 110) + #REMOVED TWO OUTLIERS
+  ggtitle("Korrelasjon mellom diameter og antall frø") +
+  theme(plot.title = element_text(size = 18, hjust = 0.5)) +
+  theme(strip.background = element_blank(), text = element_text(size = 25, hjust = 0.5))+
+  geom_hline(yintercept=60, linetype="dashed", color = "black", size=0.5) +
+  geom_hline(yintercept=69.5, linetype="dashed", color = "black", size=0.5) +#line taken from internal apple quality measurement assesment
+  geom_hline(yintercept=70, linetype="solid", color = "black", size=0.5) +
+  geom_hline(yintercept=90, linetype="solid", color = "black", size=0.5) +
+  facet_wrap(~ Apple_variety) +
+  theme(legend.position = "bottom", 
+        legend.title = element_text(size = 15),
+        legend.text = element_text (size = 12),
+        axis.text = element_text (size = 18),
+        axis.title = element_text (size = 18),
+        plot.title = element_text (hjust = 0.5, size = 20, face = "bold"))
+ggsave(AppleDiameterSeeds, filename = "Figures/Apple_Diameter_Seeds.jpeg", height = 6, width = 8)
+
+
+AppleDiameterSeeds_Location <-AppleQuality %>% 
+  ggplot(aes(y = Diameter, x = Seeds_fully_developed, color = Apple_variety)) +
+  geom_point(alpha = 0.3, position = position_jitter()) +
+  labs(x = "Antall frø", y = "Diameter (mm)", color = "Eplesort", fill = "Eplesort") +
+  geom_smooth(method = lm, fullrange=FALSE, aes(fill=Apple_variety)) + #remove line with se=FALSE, small dataset remove method=lm, 
+  theme_classic() +
   scale_color_manual (values = c("#996600", "#336633", "#CC3300")) +
   scale_fill_manual(values = c("#996600", "#336633", "#CC3300")) +
   ylim(30, 110) + #REMOVED TWO OUTLIERS
   ggtitle("Korrelasjon mellom diameter og antall frø") +
   theme(plot.title = element_text(size = 18, hjust = 0.5)) +
   theme(strip.background = element_blank(), text = element_text(size = 25, hjust = 0.5))+
-  geom_hline(yintercept=60, linetype="dashed", color = "grey", size=1) + #line taken from internal apple quality measurement asssessment
-  facet_wrap(~ Apple_variety)
-ggsave(AppleDiameterSeeds, filename = "Figures/Apple_Diameter_Seeds.jpeg", height = 6, width = 8)
-
+  geom_hline(yintercept=60, linetype="dashed", color = "black", size=0.5) +
+  geom_hline(yintercept=69.5, linetype="dashed", color = "black", size=0.5) +#line taken from internal apple quality measurement assesment
+  geom_hline(yintercept=70, linetype="solid", color = "black", size=0.5) +
+  geom_hline(yintercept=90, linetype="solid", color = "black", size=0.5) +
+  facet_wrap(~ Location) +
+  theme(legend.position = "bottom", 
+        legend.title = element_text(size = 15),
+        legend.text = element_text (size = 12),
+        axis.text = element_text (size = 18),
+        axis.title = element_text (size = 18),
+        plot.title = element_text (hjust = 0.5, size = 20, face = "bold"))
+ggsave(AppleDiameterSeeds_Location, filename = "Figures/Apple_Diameter_Seeds_Location.jpeg", height = 6, width = 8)
